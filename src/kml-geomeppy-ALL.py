97d12284332080458546ecf22a8d5ec465a00836
Coroush <soroush.garivani@iaac.net>
2019-08-30 11:48:44 +0200

    read kml and convert to geomeppy idf for H and L blocks

---
+++ src/kml-geomeppy-ALL.py
@@ -0,0 +1,212 @@
+import glob
+import os
+import xml.etree.ElementTree as et
+from xml.etree.ElementTree import tostring
+import pyproj
+from geomeppy import IDF
+
+# DEFINE HERE THE MAIN PATH
+path = r'C:\Users\Coroush\Desktop\Noumena\bcn-energy-github\190531-Files\eixample-sample1'
+
+def find_paths(res):  # block resolution, 'H' for High or 'L' for Low resolution
+    list = []
+    files = glob.glob(os.path.join(path, '*%s.kml' % res))
+    for i in files:
+        list.append(i)
+    return list
+
+def get_roots(paths):  # Reads the paths and returns the list of the kml file as et elements
+    roots = []
+    for i in paths:
+        tree = et.parse(i)
+        root = tree.getroot()
+        roots.append(root)
+    return roots
+
+def gps_to_xy(lon, lat, z):
+    crs_wgs = pyproj.Proj(init='epsg:4326')  # assuming you're using WGS84 geographic
+    crs_bng = pyproj.Proj(
+        init='epsg:5649')  # use a locally appropriate projected CRS https://epsg.io/map#srs=5649&x=31431725.375401&y=4583225.826214&z=13&layer=streets
+    x, y = pyproj.transform(crs_wgs, crs_bng, lon, lat)
+    return x, y, z
+
+def extract_polygon_elements(root):
+    elements = []
+    folders = root.findall(".//{http://www.opengis.net/kml/2.2}Folder")  # finds folder elements
+    for folder in folders:
+        placemarks = folder.findall(
+            ".//{http://www.opengis.net/kml/2.2}Placemark")  # finds placemark elements in each folder element
+        placemark_elements = []
+        for placemark in placemarks:
+            polygons = placemark.findall(
+                ".//{http://www.opengis.net/kml/2.2}Polygon")  # finds polygons in each placemark element
+            elements_polygon = []
+            for polygon in polygons:
+                coords = polygon.findall(
+                    ".//{http://www.opengis.net/kml/2.2}coordinates")  # finds the coordinates elements in each polygon element
+                elements_polygon.append(coords[0])
+            placemark_elements.append(elements_polygon)
+        elements.append((placemark_elements))
+    return elements
+
+def element_to_coordinates(coords):
+    coords_str = tostring(coords, encoding='utf8', method='xml').decode("utf-8")  # converts the element to a string
+    coords_str = coords_str.replace('<', '>')
+    text = coords_str.split('>')[4]  # takes the coordinates numbers from the string as one line
+    text = text.replace('\n', '')
+    text = text.replace('\t', '')
+    coordinate = text.split(' ')  # converts the coordinates string line to the list of 3 numbers
+    coordinate = list(filter(None, coordinate))
+    coordinates_list = []
+    for i in coordinate:
+        i = i.split(',')
+        gps = []
+        for j in i:
+            gps.append(float(j))
+        num = gps_to_xy(gps[0], gps[1], gps[2])
+        if num not in coordinates_list:
+            coordinates_list.append(num)
+    return coordinates_list
+
+def add_H_block (block_coordinates):
+    for i in block_coordinates:
+        polygon_xy = []
+        polygon_z = []
+        for point in i:
+            polygon_xy.append((point[0],point[1]))
+            polygon_z.append(point[2])
+        idf.add_block(
+            name="block_name", coordinates=polygon_xy, height=polygon_z[0]
+            )
+
+def make_L_blocks(polygon_coordinates):
+    for index in range(len(polygon_coordinates)):
+        block_name = 'L_block' + str(index)
+        polygon_xy = []
+        polygon_z = []
+        for point in polygon_coordinates[index]:
+            polygon_xy.append((point[0],point[1]))
+            polygon_z.append(point[2])
+        idf.add_block(
+            name=block_name, coordinates=polygon_xy, height=polygon_z[0]
+        )
+
+
+IDF.setiddname('C:\EnergyPlusV9-1-0\Energy+.idd')
+idf = IDF('C:\EnergyPlusV9-1-0\ExampleFiles\Minimal.idf')
+idf.epw = 'USA_CA_San.Francisco.Intl.AP.724940_TMY3.epw'
+
+#######################################################################################################################
+# Making H building
+
+# Extracting the elements
+paths_H = find_paths('H')
+H_root = get_roots(paths_H)[0]
+polygon_element = extract_polygon_elements(H_root)
+
+# Dividing the elements into two lists of blocks and surfaces
+blocks_elements = []
+surface_elements = []
+for i in polygon_element:
+    for j in i:
+        if len(j) > 1:
+            surface_elements.append(j)
+        else:
+            blocks_elements.append(j[0])
+
+# Putting each zone/block into one list
+group_surface = []
+for elements in surface_elements:
+    surface_coordinates = []
+    for element in elements:
+        points = element_to_coordinates(element)
+        surface_coordinates.append(points)
+    group_surface.append(surface_coordinates)
+
+
+# Extracting the slabs from each block
+slabs_groups = []
+for surfaces in group_surface:
+    slabs = []
+    for points in surfaces:
+        if len(points)>2:
+            polygon_z_average = 0
+            for point in points:
+                polygon_z_average += point[2]
+            polygon_z_average /= len(points)
+            if points[0][2] == polygon_z_average:
+                slabs.append(points)
+    slabs_groups.append(slabs)
+
+# Separating balconies from apartments
+apartments = []
+balconies = []
+for slab in slabs_groups:
+    if len(slab) == 1:
+        balconies.append(slab)
+    elif len(slab) == 2:
+        apartments.append(slab)
+    else:
+        print ("I don't know if its a balcony or apartment")
+
+# Extracting the floor coordinates and height
+floor_coordinates = []
+floor_heights = []
+floor_z = []
+for apartment in apartments:
+    floor_coordinates.append(apartment[0])
+    z_value = apartment[1][0][2] - apartment[0][0][2]
+    floor_heights.append(z_value)
+    floor_z.append(apartment[0][0][2])
+
+# creating groups based on floor z values so the polygons in the same height will be in the same group and then they will all move up together
+floor_index = []
+dup = {}
+for i in range(len(floor_z)):
+    if floor_z[i] not in dup.values():
+        if len(dup) > 0:
+            floor_index.append(dup)
+        dup = []
+        dup.append(i)
+    if floor_z
+        dup.append(i)
+
+print (floor_index)
+
+
+
+
+
+
+# for i in range(len(apartments)):
+#     idf.add_block(
+#         name = 'Storey' + str(i),
+#         coordinates = floor_coordinates[i],
+#         height = floor_heights[i]
+#     )
+
+#######################################################################################################################
+# Making L blocks
+
+# paths_L = find_paths('L')
+# L_roots = get_roots(paths_L)
+#
+# L_elements = []
+# for root in L_roots:
+#     L_elements.append(extract_polygon_elements(root))
+#
+# L_polygons = []
+# for i in L_elements:
+#     for j in i:
+#         for k in j:
+#             L_polygons.append(element_to_coordinates(k[0]))
+# # print (L_polygons[:2])
+# make_L_blocks(L_polygons[:1])
+
+#######################################################################################################################
+
+# idf.translate_to_origin()
+# idf.set_default_constructions()
+# idf.intersect_match()
+# idf.view_model()
+# idf.run()
