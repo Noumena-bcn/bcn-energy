5de045ab4e12d68a087361825122b5fc10f59d33
Coroush <45938250+Coroush@users.noreply.github.com>
2019-09-09 12:39:55 +0200

    Update windows

--- src/kml-geomeppy-ALL.py
+++ src/kml-geomeppy-ALL.py
@@ -8,9 +8,9 @@ from geomeppy.geom.polygons import intersect, unique, Polygon3D
 from collections import defaultdict
 from geomeppy.utilities import almostequal
 
-
 # DEFINE HERE THE MAIN PATH
-path = '/Users/soroush/Desktop/Noumena/eixample-sample1'
+# path = '/Users/soroush/Desktop/Noumena/eixample-sample1'
+path = r'C:\Users\Coroush\Desktop\Noumena\bcn-energy-github\190531-Files\eixample-sample1'
 
 def find_paths(res):  # block resolution, 'H' for High or 'L' for Low resolution
     list = []
@@ -18,6 +18,7 @@ def find_paths(res):  # block resolution, 'H' for High or 'L' for Low resolution
     for i in files:
         list.append(i)
     return list
+
 
 def get_roots(paths):  # Reads the paths and returns the list of the kml file as et elements
     roots = []
@@ -27,12 +28,14 @@ def get_roots(paths):  # Reads the paths and returns the list of the kml file as
         roots.append(root)
     return roots
 
+
 def gps_to_xy(lon, lat, z):
     crs_wgs = pyproj.Proj(init='epsg:4326')  # assuming you're using WGS84 geographic
     crs_bng = pyproj.Proj(
         init='epsg:5649')  # use a locally appropriate projected CRS https://epsg.io/map#srs=5649&x=31431725.375401&y=4583225.826214&z=13&layer=streets
     x, y = pyproj.transform(crs_wgs, crs_bng, lon, lat)
     return x, y, z
+
 
 def extract_polygon_elements(root):
     elements = []
@@ -53,6 +56,7 @@ def extract_polygon_elements(root):
         elements.append((placemark_elements))
     return elements
 
+
 def element_to_coordinates(coords):
     coords_str = tostring(coords, encoding='utf8', method='xml').decode("utf-8")  # converts the element to a string
     coords_str = coords_str.replace('<', '>')
@@ -72,16 +76,18 @@ def element_to_coordinates(coords):
             coordinates_list.append(num)
     return coordinates_list
 
-def add_H_block (block_coordinates):
+
+def add_H_block(block_coordinates):
     for i in block_coordinates:
         polygon_xy = []
         polygon_z = []
         for point in i:
-            polygon_xy.append((point[0],point[1]))
+            polygon_xy.append((point[0], point[1]))
             polygon_z.append(point[2])
         idf.add_block(
             name="block_name", coordinates=polygon_xy, height=polygon_z[0]
-            )
+        )
+
 
 def make_L_blocks(polygon_coordinates):
     for index in range(len(polygon_coordinates)):
@@ -90,7 +96,7 @@ def make_L_blocks(polygon_coordinates):
         polygon_xy = []
         polygon_z = []
         for point in polygon_coordinates[index]:
-            polygon_xy.append((point[0],point[1]))
+            polygon_xy.append((point[0], point[1]))
             polygon_z.append(point[2])
         idf.add_shading_block(
             name=block_name,
@@ -99,12 +105,15 @@ def make_L_blocks(polygon_coordinates):
         )
         shading_roof = idf.newidfobject(
             'SHADING:SITE:DETAILED',
-            Name = roof_name
+            Name=roof_name
         )
         shading_roof.setcoords(polygon_coordinates[index])
 
-IDF.setiddname('/Applications/EnergyPlus-8-8-0/Energy+.idd')
-idf = IDF('/Applications/EnergyPlus-8-8-0/ExampleFiles/Minimal.idf')
+
+# IDF.setiddname('/Applications/EnergyPlus-8-8-0/Energy+.idd')
+# idf = IDF('/Applications/EnergyPlus-8-8-0/ExampleFiles/Minimal.idf')
+IDF.setiddname("C:/EnergyPlusV9-1-0/Energy+.idd")
+idf = IDF("C:/EnergyPlusV9-1-0/ExampleFiles/Minimal.idf")
 idf.epw = 'USA_CA_San.Francisco.Intl.AP.724940_TMY3.epw'
 
 #######################################################################################################################
@@ -139,7 +148,7 @@ slabs_groups = []
 for surfaces in group_surface:
     slabs = []
     for points in surfaces:
-        if len(points)>2:
+        if len(points) > 2:
             polygon_z_average = 0
             for point in points:
                 polygon_z_average += point[2]
@@ -157,7 +166,7 @@ for slab in slabs_groups:
     elif len(slab) == 2:
         apartments.append(slab)
     else:
-        print ("I don't know if its a balcony or apartment")
+        print("I don't know if its a balcony or apartment")
 
 # Extracting the floor coordinates and height
 floor_coordinates = []
@@ -187,12 +196,12 @@ for i in range(len(floor_z)):
 for level in levels:
     for i in level:
         idf.add_block(
-            name = str(i),
-            coordinates = floor_coordinates[i],
-            height = floor_heights[i]
+            name=str(i),
+            coordinates=floor_coordinates[i],
+            height=floor_heights[i]
         )
-    idf.translate([0,0,3])
-idf.translate([0,0,-3])
+    idf.translate([0, 0, 3])
+idf.translate([0, 0, -3])
 
 #######################################################################################################################
 # Making L blocks
@@ -212,18 +221,19 @@ for i in L_elements:
 
 make_L_blocks(L_polygons)
 
+
 #######################################################################################################################
 
 def move_to_origin():
     n = 0
-    h_center = [0,0,0]
+    h_center = [0, 0, 0]
     for i in floor_coordinates:
         for j in i:
-            n = n+1
+            n = n + 1
             h_center[0] = h_center[0] + j[0]
             h_center[1] = h_center[1] + j[1]
     for k in range(2):
-        h_center[k] = h_center[k]/(-n)
+        h_center[k] = h_center[k] / (-n)
 
     idf.translate(h_center)
 
@@ -232,15 +242,12 @@ idf.set_default_constructions()
 idf.match()
 
 #######################################################################################################################
-def get_centroid(coords): # coords is a list of coordinates of surface
-    srf_center = [0, 0, 0]
-    for coord in coords:
-        srf_center[0] = srf_center[0] + coord[0]
-        srf_center[1] = srf_center[1] + coord[1]
-        srf_center[2] = srf_center[2] + coord[2]
-    for i in range(3):
-        srf_center[i] = srf_center[i] / 4
-    return srf_center
+def populate_adjacencies(s1, s2):
+    poly1 = Polygon3D(s1.coords)
+    poly2 = Polygon3D(s2.coords)
+    if almostequal(abs(poly1.distance), abs(poly2.distance), 2):
+        if almostequal(poly1.normal_vector, poly2.normal_vector, 2) or almostequal(poly1.normal_vector, -poly2.normal_vector, 2):
+                return True
 
 idf.set_wwr(
     wwr=0.4,
@@ -249,85 +256,29 @@ idf.set_wwr(
 
 shading_srfs = idf.getshadingsurfaces()
 block_srfs = idf.getsurfaces("Wall")
+windows = idf.getsubsurfaces("window")
 
-adj = defaultdict(list)
+adj_walls = []
+for i in range(len(block_srfs)):
+    for j in shading_srfs:
+        ad = (populate_adjacencies(block_srfs[i],j))
+        if ad:
+            adj_walls.append (block_srfs[i])
+            break
 
-def populate_adjacencies(adjacencies, s1, s2):
-    poly1 = Polygon3D(s1.coords)
-    poly2 = Polygon3D(s2.coords)
+m = 0
+for i in range(len(windows)):
+    a = windows[i]
+    for b in adj_walls:
+        if b.Name in a.Name:
+            idf.popidfobject("FENESTRATIONSURFACE:DETAILED",i-m)
+            m += 1
+            break
 
-    file.write(str(poly1))
-    file.write('\n')
-    file.write(str(poly2))
-    file.write('\n')
-    file.write(str(poly1.distance))
-    file.write('\n')
-    file.write(str(poly2.distance))
-    file.write('\n')
+########################################################################################################################
 
-    if not almostequal(abs(poly1.distance), abs(poly2.distance), 4):
-        # print("not equal distance")
-        return adjacencies
-    if not almostequal(poly1.normal_vector, poly2.normal_vector, 4):
-        if not almostequal(poly1.normal_vector, -poly2.normal_vector, 4):
-            # print("not equal vector")
-            return adjacencies
-
-
-    intersection = poly1.intersect(poly2)
-    if intersection:
-        new_surfaces = intersect(poly1, poly2)
-        new_s1 = [
-            s
-            for s in new_surfaces
-            if almostequal(s.normal_vector, poly1.normal_vector, 7)
-        ]
-        new_s2 = [
-            s
-            for s in new_surfaces
-            if almostequal(s.normal_vector, poly2.normal_vector, 7)
-        ]
-        adjacencies[(s1.key, s1.Name)] += new_s1
-        adjacencies[(s2.key, s2.Name)] += new_s2
-    return adjacencies
-
-
-file = open('coordinates.txt','w')
-
-for i in block_srfs:
-    poly1 = Polygon3D(i.coords)
-    file.write(str(poly1))
-    file.write('\n')
-    # for j in shading_srfs:
-    #     poly2 = Polygon3D(j.coords)
-        # print (poly1.is_coplanar(poly2))
-        # print (i.Name, j.Name)
-        # ad = (populate_adjacencies(adj,i,j))
-        # for surface in ad:
-        #     ad[surface] = unique(ad[surface])
-
-file.close()
-
-# print(ad)
-
-# keys_list = ad.keys()
-# print(keys_list)
-# intersecting_walls = []
-# for a in keys_list:
-#     for b in a:
-#         if "Storey" in b:
-#             intersecting_walls.append(b)
-#
-# print (intersecting_walls)
-
-
-# m = 0
-# for i in shadows:
-#     idf.popidfobject("FENESTRATIONSURFACE:DETAILED",i-m)
-#     m += 1
-#
-# #######################################################################################################################
-#
-# idf.to_obj("allblocks.obj")
-# # idf.view_model()
-# # idf.run()
+idf.set_default_constructions()
+idf.match()
+idf.to_obj("allblocks.obj")
+# idf.view_model()
+idf.run()
