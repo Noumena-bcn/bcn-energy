bf3fed7ff551d30117ec7800a13d1b8bd933dc5c
coroush <soroush.garivani@iaac.net>
2019-09-13 11:31:03 +0200

    new test file for H block

--- src/test-zones.py
+++ src/test-zones.py
@@ -74,30 +74,152 @@ def element_to_coordinates(coords):
         gps = []
         for j in i:
             gps.append(float(j))
-        num = gps_to_xy(gps[0], gps[1], gps[2])
+        num = gps_to_xy(gps[0], gps[1],gps[2])
         if num not in coordinates_list:
             coordinates_list.append(num)
     return coordinates_list
 
+def make_L_blocks(polygon_coordinates):
+    for index in range(len(polygon_coordinates)):
+        block_name = 'L_block' + str(index)
+        roof_name = 'Shading' + str(index)
+        polygon_xy = []
+        polygon_z = []
+        for point in polygon_coordinates[index]:
+            polygon_xy.append((point[0], point[1]))
+            polygon_z.append(point[2])
+        idf.add_shading_block(
+            name=block_name,
+            coordinates=polygon_xy,
+            height=polygon_z[0]
+        )
+        shading_roof = idf.newidfobject(
+            'SHADING:SITE:DETAILED',
+            Name=roof_name
+        )
+        shading_roof.setcoords(polygon_coordinates[index])
+
+def move_to_origin(floor_coordinates):
+    n = 0
+    h_center = [0, 0, 0]
+    for j in floor_coordinates:
+        n = n + 1
+        h_center[0] = h_center[0] + int(j[0])
+        h_center[1] = h_center[1] + int(j[1])
+    for k in range(2):
+        h_center[k] = h_center[k] / (-n)
+    idf.translate(h_center)
+
+def populate_adjacencies(s1, s2):
+    poly1 = Polygon3D(s1.coords)
+    poly2 = Polygon3D(s2.coords)
+    if almostequal(abs(poly1.distance), abs(poly2.distance), 3):
+        if almostequal(poly1.normal_vector, poly2.normal_vector, 3) or almostequal(poly1.normal_vector,
+                                                                                   -poly2.normal_vector, 3):
+            return True
+
+#######################################################################################################################
+# DEFINE HERE THE MAIN PATH
 
 path = '/Users/soroush/Desktop/Noumena/eixample-sample1'
+# path = r'C:\Users\Coroush\Desktop\Noumena\bcn-energy-github\190531-Files\eixample-sample1'
+
+IDF.setiddname('/Applications/EnergyPlus-8-8-0/Energy+.idd')
+idf = IDF('/Applications/EnergyPlus-8-8-0/ExampleFiles/Minimal.idf')
+# IDF.setiddname("C:/EnergyPlusV9-1-0/Energy+.idd")
+# idf = IDF("C:/EnergyPlusV9-1-0/ExampleFiles/Minimal.idf")
+
+idf.epw = 'USA_CA_San.Francisco.Intl.AP.724940_TMY3.epw'
+
+#######################################################################################################################
+# Making H building
 
 # Extracting the elements
 paths_H = find_paths('H')
 H_root = get_roots(paths_H)[0]
-polygons = extract_polygon_elements(H_root)
+polygons = extract_polygon_elements(H_root)[1:-1]
+polygons.reverse()
 
 folders = []
+coordinates = []
+
 for folder in polygons:
     placemarks = []
     for placemark in folder:
         if placemark [0] != "Terraza":
-            placemark [2] = element_to_coordinates(placemark[2])
-            placemarks.append(placemark[0:3])
+            placemark[2] = element_to_coordinates(placemark[2])
+            placemark[2] = [row [0:2] for row in placemark[2]]
+            if placemark[0:3] not in placemarks:
+                placemarks.append(placemark[0:3])
+                for i in placemark[2]:
+                    coordinates.append(i)
     folders.append(placemarks)
 
+for folder in range(len(folders)):
+    for placemark in range(len(folders[folder])):
+        b_name = "level " + str(len(folders)-folder) + " unit " + str(placemark)
+        idf.add_block(
+            name=b_name,
+            coordinates=folders[folder][placemark][2],
+            height=3
+        )
+    idf.translate([0,0,3])
+idf.translate([0,0,-3])
 
-for folder in folders:
-    print ("new folder")
-    for placemark in folder:
-        print (placemark)
+#######################################################################################################################
+# Making L blocks
+
+paths_L = find_paths('L')
+L_roots = get_roots(paths_L)
+
+L_elements = []
+for root in L_roots:
+    L_elements.append(extract_polygon_elements(root)[0])
+
+L_polygons = []
+for i in L_elements:
+    for j in i:
+        L_polygons.append(element_to_coordinates(j[2]))
+
+make_L_blocks(L_polygons)
+
+#######################################################################################################################
+# Move to origin, default construction and intersect match
+
+move_to_origin(coordinates)
+idf.set_default_constructions()
+idf.match()
+
+#######################################################################################################################
+# adding external windows for non adjacent to shading blocks
+
+idf.set_wwr(
+    wwr=0.4,
+    construction="Project External Window")
+
+shading_srfs = idf.getshadingsurfaces()
+block_srfs = idf.getsurfaces("Wall")
+windows = idf.getsubsurfaces("window")
+
+adj_walls = []
+for i in range(len(block_srfs)):
+    for j in shading_srfs:
+        ad = (populate_adjacencies(block_srfs[i],j))
+        if ad:
+            adj_walls.append (block_srfs[i])
+            break
+
+m = 0
+for i in range(len(windows)):
+    a = windows[i]
+    for b in adj_walls:
+        if b.Name in a.Name:
+            idf.popidfobject("FENESTRATIONSURFACE:DETAILED",i-m)
+            m += 1
+            break
+
+#######################################################################################################################
+
+idf.to_obj("newblocks2.obj")
+idf.view_model()
+idf.run()
