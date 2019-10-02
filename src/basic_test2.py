a546b9c933d908906d30c3f78f12fa175fbee893
coroush <soroush.garivani@iaac.net>
2019-10-02 19:06:13 +0200

    custom wwr

--- src/basic_test2.py
+++ src/basic_test2.py
@@ -1,4 +1,5 @@
 from geomeppy import IDF
+from geomeppy.geom.polygons import Polygon3D
 
 IDF.setiddname('/Applications/EnergyPlus-8-8-0/Energy+.idd')
 idf = IDF('/Users/soroush/Desktop/Noumena/bcn-energy/src/gh_template.idf')
@@ -32,8 +33,8 @@ idf.add_block(
     height=3
 )
 
-# idf.intersect_match()
-# idf.set_default_constructions()
+idf.intersect_match()
+
 
 zones = idf.idfobjects["ZONE"]
 target_zone_names = ["vivienda0", "vivienda1", "comercio0", "comercio1"]
@@ -47,12 +48,101 @@ for srf in srfs:
         if target_zone_names[index] in srf.Zone_Name:
             srf.Zone_Name = target_zone_names[index]
 
+wwr_zones = {"vivienda":0.2, "comercio":0.7, "comun":0.2}
+
+def _is_window(subsurface):
+    if subsurface.key.lower() in {"window", "fenestrationsurface:detailed"}:
+        return True
+
+def window_vertices_given_wall(wall, wwr):
+    # type: (EpBunch, float) -> Polygon3D
+    """Calculate window vertices given wall vertices and glazing ratio.
+    :: For each axis:
+        1) Translate the axis points so that they are centred around zero
+        2) Either:
+            a) Multiply the z dimension by the glazing ratio to shrink it vertically
+            b) Multiply the x or y dimension by 0.995 to keep inside the surface
+        3) Translate the axis points back to their original positions
+    :param wall: The wall to add a window on. We expect each wall to have four vertices.
+    :param wwr: Window to wall ratio.
+    :returns: Window vertices bounding a vertical strip midway up the surface.
+    """
+    vertices = wall.coords
+    average_x = sum([x for x, _y, _z in vertices]) / len(vertices)
+    average_y = sum([y for _x, y, _z in vertices]) / len(vertices)
+    average_z = sum([z for _x, _y, z in vertices]) / len(vertices)
+    # move windows in 0.5% from the edges so they can be drawn in SketchUp
+    window_points = [
+        [
+            ((x - average_x) * 0.999) + average_x,
+            ((y - average_y) * 0.999) + average_y,
+            ((z - average_z) * wwr) + average_z,
+        ]
+        for x, y, z in vertices
+    ]
+
+    return Polygon3D(window_points)
+
+def custom_wwr (wwr_zones, construction=None):
+
+    try:
+        ggr = idf.idfobjects["GLOBALGEOMETRYRULES"][0]  # type: Optional[Idf_MSequence]
+    except IndexError:
+        ggr = None
+    external_walls = filter(
+        lambda x: x.Outside_Boundary_Condition.lower() == "outdoors",
+        idf.getsurfaces("wall"),
+    )
+    subsurfaces = idf.getsubsurfaces()
+
+    for wall in external_walls:
+        # get any subsurfaces on the wall
+        wall_subsurfaces = list(
+            filter(lambda x: x.Building_Surface_Name == wall.Name, subsurfaces)
+        )
+        if not all(_is_window(wss) for wss in wall_subsurfaces) and not force:
+            raise ValueError(
+                'Not all subsurfaces on wall "{name}" are windows. '
+                "Use `force=True` to replace all subsurfaces.".format(name=wall.Name)
+            )
+
+        if wall_subsurfaces and not construction:
+            constructions = list(
+                {wss.Construction_Name for wss in wall_subsurfaces if _is_window(wss)}
+            )
+            if len(constructions) > 1:
+                raise ValueError(
+                    'Not all subsurfaces on wall "{name}" have the same construction'.format(
+                        name=wall.Name
+                    )
+                )
+            construction = constructions[0]
+        # remove all subsurfaces
+        for ss in wall_subsurfaces:
+            idf.removeidfobject(ss)
+
+        for i in wwr_zones.keys():
+            if i in wall.Zone_Name:
+                name = i
+                wwr = wwr_zones[name]
+
+        if not wwr:
+            print ("error in adding windows")
+            return
+
+        coords = window_vertices_given_wall(wall, wwr)
+        window = idf.newidfobject(
+                    "FENESTRATIONSURFACE:DETAILED",
+                    Name="%s window" % wall.Name,
+                    Surface_Type="Window",
+                    Construction_Name=construction or "",
+                    Building_Surface_Name=wall.Name,
+                    View_Factor_to_Ground="autocalculate")
+        window.setcoords(coords, ggr)
 
 
+idf.set_default_constructions()
+custom_wwr(wwr_zones, construction="Project External Window")
+idf.to_obj('test2.obj')
 
-
-srfs = idf.getsurfaces()
-for i in srfs:
-    print (i.Name)
-    print (i.coords)
-    print (i.Zone_Name)
+idf.run()
--- src/test-zones.py
+++ src/test-zones.py
@@ -6,6 +6,7 @@ import pyproj
 from geomeppy import IDF
 from geomeppy.geom.polygons import Polygon3D
 from geomeppy.utilities import almostequal
+from geomeppy.geom.polygons import Polygon3D
 
 #######################################################################################################################
 
@@ -120,7 +121,6 @@ def populate_adjacencies(s1, s2):
                                                                                    -poly2.normal_vector, 3):
             return True
 
-
 def add_electric_equipment (zone_name):
     if "vivienda" in zone_name:
         idf.newidfobject("ELECTRICEQUIPMENT",
@@ -217,6 +217,96 @@ def add_outdoor_air (zone_name):
                          Outdoor_Air_Method="Sum",
                          Outdoor_Air_Flow_per_Person=0.00353925,
                          Outdoor_Air_Flow_per_Zone_Floor_Area=0.0006096012)
+
+def _is_window(subsurface):
+    if subsurface.key.lower() in {"window", "fenestrationsurface:detailed"}:
+        return True
+
+def window_vertices_given_wall(wall, wwr):
+    # type: (EpBunch, float) -> Polygon3D
+    """Calculate window vertices given wall vertices and glazing ratio.
+    :: For each axis:
+        1) Translate the axis points so that they are centred around zero
+        2) Either:
+            a) Multiply the z dimension by the glazing ratio to shrink it vertically
+            b) Multiply the x or y dimension by 0.995 to keep inside the surface
+        3) Translate the axis points back to their original positions
+    :param wall: The wall to add a window on. We expect each wall to have four vertices.
+    :param wwr: Window to wall ratio.
+    :returns: Window vertices bounding a vertical strip midway up the surface.
+    """
+    vertices = wall.coords
+    average_x = sum([x for x, _y, _z in vertices]) / len(vertices)
+    average_y = sum([y for _x, y, _z in vertices]) / len(vertices)
+    average_z = sum([z for _x, _y, z in vertices]) / len(vertices)
+    # move windows in 0.5% from the edges so they can be drawn in SketchUp
+    window_points = [
+        [
+            ((x - average_x) * 0.999) + average_x,
+            ((y - average_y) * 0.999) + average_y,
+            ((z - average_z) * wwr) + average_z,
+        ]
+        for x, y, z in vertices
+    ]
+
+    return Polygon3D(window_points)
+
+def custom_wwr (wwr_zones, construction=None):
+
+    try:
+        ggr = idf.idfobjects["GLOBALGEOMETRYRULES"][0]  # type: Optional[Idf_MSequence]
+    except IndexError:
+        ggr = None
+    external_walls = filter(
+        lambda x: x.Outside_Boundary_Condition.lower() == "outdoors",
+        idf.getsurfaces("wall"),
+    )
+    subsurfaces = idf.getsubsurfaces()
+
+    for wall in external_walls:
+        # get any subsurfaces on the wall
+        wall_subsurfaces = list(
+            filter(lambda x: x.Building_Surface_Name == wall.Name, subsurfaces)
+        )
+        if not all(_is_window(wss) for wss in wall_subsurfaces) and not force:
+            raise ValueError(
+                'Not all subsurfaces on wall "{name}" are windows. '
+                "Use `force=True` to replace all subsurfaces.".format(name=wall.Name)
+            )
+
+        if wall_subsurfaces and not construction:
+            constructions = list(
+                {wss.Construction_Name for wss in wall_subsurfaces if _is_window(wss)}
+            )
+            if len(constructions) > 1:
+                raise ValueError(
+                    'Not all subsurfaces on wall "{name}" have the same construction'.format(
+                        name=wall.Name
+                    )
+                )
+            construction = constructions[0]
+        # remove all subsurfaces
+        for ss in wall_subsurfaces:
+            idf.removeidfobject(ss)
+
+        for i in wwr_zones.keys():
+            if i in wall.Zone_Name:
+                name = i
+                wwr = wwr_zones[name]
+
+        if not wwr:
+            print ("error in adding windows")
+            return
+
+        coords = window_vertices_given_wall(wall, wwr)
+        window = idf.newidfobject(
+                    "FENESTRATIONSURFACE:DETAILED",
+                    Name="%s window" % wall.Name,
+                    Surface_Type="Window",
+                    Construction_Name=construction or "",
+                    Building_Surface_Name=wall.Name,
+                    View_Factor_to_Ground="autocalculate")
+        window.setcoords(coords, ggr)
 
 #######################################################################################################################
 # DEFINE HERE THE MAIN PATH
@@ -325,9 +415,9 @@ idf.match()
 #######################################################################################################################
 # adding external windows for non adjacent to shading blocks
 
-idf.set_wwr(
-    wwr=0.4,
-    construction="Project External Window")
+wwr_zones = {"vivienda":0.2, "comercio":0.7, "comun":0.2}
+custom_wwr(wwr_zones, construction="Project External Window")
+
 
 shading_srfs = idf.getshadingsurfaces()
 block_srfs = idf.getsurfaces("Wall")
@@ -349,6 +439,8 @@ for i in range(len(windows)):
             idf.popidfobject("FENESTRATIONSURFACE:DETAILED",i-m)
             m += 1
             break
+
+
 
 #######################################################################################################################
 
